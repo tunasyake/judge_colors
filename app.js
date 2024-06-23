282a0b49f759f52ee185163ae839700bad1236e9
tunasyake <naotti0417@gmail.com>
2024-06-23 22:57:30 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,395 @@
+
+//問題設定
+quiz = [
+  {
+      "question" : "Fe²⁺",
+      "correct" : [
+          "淡緑色"
+      ]
+  },
+  {
+      "question" : "Fe(OH)₂",
+      "correct" : [
+          "緑白色"
+      ]
+  },
+  {
+      "question" : "K₄[Fe(CN)₆]",
+      "correct" : [
+          "濃青色"
+      ]
+  },
+  {
+      "question" : "Fe³⁺",
+      "correct" : [
+          "黄褐色"
+      ]
+  },
+  {
+      "question" : "Fe(OH)₃",
+      "correct" : [
+          "赤褐色"
+      ]
+  },
+  {
+      "question" : "K₃[Fe(CN)₆]",
+      "correct" : [
+          "濃青色"
+      ]
+  },
+  {
+      "question" : "KSCN",
+      "correct" : [
+          "血赤色"
+      ]
+  },
+  {
+      "question" : "Cu₂O",
+      "correct" : [
+          "赤色"
+      ]
+  },
+  {
+      "question" : "CuO",
+      "correct" : [
+          "黒色"
+      ]
+  },
+  {
+      "question" : "CuSO₄・5H₂O",
+      "correct" : [
+          "青色"
+      ]
+  },
+  {
+      "question" : "CuSO₄",
+      "correct" : [
+          "白色"
+      ]
+  },
+  {
+      "question" : "Cu²⁺",
+      "correct" : [
+          "青色"
+      ]
+  },
+  {
+      "question" : "Cu(OH)₂",
+      "correct" : [
+          "青白色"
+      ]
+  },
+  {
+      "question" : "[Cu(NH₃)₄]²⁺",
+      "correct" : [
+          "深青色"
+      ]
+  },
+  {
+      "question" : "CuS",
+      "correct" : [
+          "黒色"
+      ]
+  },
+  {
+      "question" : "Cu₂[Fe(CN)₆]",
+      "correct" : [
+          "赤褐色"
+      ]
+  },
+  {
+      "question" : "CuSO₄・7H₂O",
+      "correct" : [
+          "青緑色"
+      ]
+  },
+  {
+      "question" : "Ag₂O",
+      "correct" : [
+          "褐色"
+      ]
+  },
+  {
+      "question" : "AgCl",
+      "correct" : [
+          "白色"
+      ]
+  },
+  {
+      "question" : "AgBr",
+      "correct" : [
+          "淡黄色"
+      ]
+  },
+  {
+      "question" : "AgI",
+      "correct" : [
+          "黄色"
+      ]
+  },
+  {
+      "question" : "Ag⁺",
+      "correct" : [
+          "無色"
+      ]
+  },
+  {
+      "question" : "[Ag(NH₃)₂]⁺",
+      "correct" : [
+          "無色"
+      ]
+  },
+  {
+      "question" : "Ag₂S",
+      "correct" : [
+          "黒色"
+      ]
+  },
+  {
+      "question" : "[Ag(S₂O₃)₂]³⁻",
+      "correct" : [
+          "無色"
+      ]
+  },
+  {
+      "question" : "[Ag(CN)₂]⁻",
+      "correct" : [
+          "無色"
+      ]
+  },
+  {
+      "question" : "Cr",
+      "correct" : [
+          "銀白色"
+      ]
+  },
+  {
+      "question" : "Cr²⁺",
+      "correct" : [
+          "青色"
+      ]
+  },
+  {
+      "question" : "Cr³⁺",
+      "correct" : [
+          "緑色"
+      ]
+  },
+  {
+      "question" : "CrO",
+      "correct" : [
+          "黒色"
+      ]
+  },
+  {
+      "question" : "Cr₂O₃",
+      "correct" : [
+          "緑色"
+      ]
+  },
+  {
+      "question" : "CrO₃",
+      "correct" : [
+          "暗赤色"
+      ]
+  },
+  {
+      "question" : "Cr(OH)₃",
+      "correct" : [
+          "灰緑色"
+      ]
+  },
+  {
+      "question" : "[Cr(OH)₄]⁻",
+      "correct" : [
+          "緑色"
+      ]
+  },
+  {
+      "question" : "K₂CrO₄²⁻",
+      "correct" : [
+          "黄色"
+      ]
+  },
+  {
+      "question" : "K₂Cr₂O₇",
+      "correct" : [
+          "赤橙色"
+      ]
+  },
+  {
+      "question" : "PbCrO₄",
+      "correct" : [
+          "黄色"
+      ]
+  },
+  {
+      "question" : "Ag₂CrO₄",
+      "correct" : [
+          "赤褐色",
+          "赤色",
+          "赤（褐）色",
+          "赤(褐)色"
+      ]
+  },
+  {
+      "question" : "BaCrO₄",
+      "correct" : [
+          "淡黄色",
+          "黄色",
+          "（淡）黄色",
+          "(淡)黄色"
+      ]
+  },
+  {
+      "question" : "Mn",
+      "correct" : [
+          "灰色"
+      ]
+  },
+  {
+      "question" : "Mn²⁺",
+      "correct" : [
+          "淡桃色"
+      ]
+  },
+  {
+      "question" : "MnO₂",
+      "correct" : [
+          "黒褐色"
+      ]
+  },
+  {
+      "question" : "Mn(OH)₂",
+      "correct" : [
+          "白色"
+      ]
+  },
+  {
+      "question" : "K₂MnO₄",
+      "correct" : [
+          "緑色"
+      ]
+  },
+  {
+      "question" : "KMnO₄",
+      "correct" : [
+          "黒紫色"
+      ]
+  },
+  {
+      "question" : "KMnO₄aq",
+      "correct" : [
+          "赤紫色"
+      ]
+  }
+]
+
+//初期化
+let quizLenght = quiz.length;
+let answerCount = 0;
+const quizMaxCount = 15;
+let quizCount = quizMaxCount;
+let quizRemain = quizCount;
+let quizChosen = 0;
+let userAnswer = "";
+let userScore = 0;
+
+const setQuestion = () => {
+  console.log('run:setQuestion');
+
+  //問題ランダム選択
+  quizChosen = Math.floor(Math.random() * (quizLenght - answerCount));
+  //問題表示
+  document.getElementById('displaytext-question').textContent = quiz[quizChosen].question
+  //カウント表示
+  document.getElementById('displaytext-count').textContent = answerCount + 1 + '/' + quizMaxCount
+};
+
+//結果モーダル表示
+const displayResult = (r) => {
+  //表示
+  document.getElementById('displaytext-result-question').textContent = quiz[quizChosen].question ;
+  document.getElementById('displaytext-result-correct').textContent = ' 〇  ' + quiz[quizChosen].correct ;
+  document.getElementById('modal-result-background').style.opacity = 1;
+  document.getElementById('modal-result-background').style.pointerEvents = '';
+  document.getElementById('modal-result-container').style.opacity = 1;
+  document.getElementById('modal-result-container').style.pointerEvents = '';
+  if(r === true){
+    //正解時
+    document.getElementById('displaytext-result-result').textContent = '正解！' ;
+    document.getElementById('displaytext-result-wrong').textContent = '' ;
+    userScore++;
+  } else{
+    //不正解時
+    document.getElementById('displaytext-result-result').textContent = '不正解...' ;
+    document.getElementById('displaytext-result-wrong').textContent = ' ×  ' + userAnswer ;
+  }
+};
+
+//最終結果
+const displayTotalScore = () => {
+  //モーダル表示
+  document.getElementById('modal-result-background').style.opacity = 1;
+  document.getElementById('modal-result-background').style.pointerEvents = '';
+  document.getElementById('modal-totalscore-container').style.opacity = 1;
+  document.getElementById('modal-totalscore-container').style.pointerEvents = '';
+  document.getElementById('displaytext-question').textContent = '結果発表'
+  document.getElementById('displaytext-totalscore-score').textContent = 'スコア：' + userScore + ' / ' + quizMaxCount;
+  if(userScore === quizMaxCount){
+    document.getElementById('displaytext-totalscore-comment').textContent = '全問正解！'
+  }else {
+    document.getElementById('displaytext-totalscore-comment').textContent = ''
+  }
+} 
+
+//正誤判定
+const checkAnswer = () => {
+  //解答なし処理
+  if(document.getElementById('textbox-answer').value === ""){
+    document.getElementById('textbox-answer').value = "解答なし"
+  };
+  userAnswer = document.getElementById('textbox-answer').value;
+  //モーダル表示
+  displayResult(quiz[quizChosen].correct.includes(userAnswer));
+  
+};
+
+//次の問題へ
+const nextQuestion = () => {
+  //モーダル非表示
+  document.getElementById('modal-result-background').style.opacity = 0;
+  document.getElementById('modal-result-background').style.pointerEvents = 'none';
+  document.getElementById('modal-result-container').style.opacity = 0;
+  document.getElementById('modal-result-container').style.pointerEvents = 'none';
+  //入力消去
+  document.getElementById('textbox-answer').value = "";
+  //問題削除
+  quiz.splice(quizChosen,1);
+  //残り問題数
+  answerCount++
+  quizRemain = quizRemain - 1;
+  //まだあるか
+  if(quizRemain > 0){
+    //問題設定
+    setQuestion();
+  } else{
+    //結果発表
+    displayTotalScore();
+  }
+};
+
+//以下実行
+
+//問題表示
+setQuestion();
+
+//解答ボタン
+document.getElementById('button-answer').addEventListener('click', () => {
+    checkAnswer();
+  });
+  
+//次の問題へ
+document.getElementById('button-result-close').addEventListener('click', () => {
+    nextQuestion();
+  });
